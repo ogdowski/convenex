fb55907020da0a64fe168bd8035aca4e802e358a
Ariel Ogdowski <ogdowski@protonmail.com>
2019-03-27 21:56:29 +0100

    add typing

--- convenex/__main__.py
+++ convenex/__main__.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
 import click
 
 
 @click.command()
 @click.option("--file", help="Path to the file you want to convert")
-def main(file):
+def main(file: str) -> None:
     """Tool for converting Evernote XML (.enex) files into Markdown (.md) files"""
     click.echo(f"{file}")
